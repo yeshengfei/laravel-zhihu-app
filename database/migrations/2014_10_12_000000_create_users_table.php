778ee34cca83ef6fb3dbe0cf1eaf65a3bf8d1cf6
yeshengfei <191705595@qq.com>
2017-01-25 00:24:01 +0800

    基础的migrate修改，并未带上settings的字段

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,21 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('avatar');
+            $table->smallInteger('confirmation_token')->default(0);
+            $table->integer('questions_count')->default(0);
+            $table->integer('answer_count')->default(0);
+            $table->integer('comments_count')->default(0);
+            $table->integer('favorites_count')->default(0);
+            $table->integer('likes_count')->default(0);
+            $table->integer('followers_count')->default(0);
+            $table->integer('followings_count')->default(0);
+//            json的数据类型，只有mysql 5.7才支持
+//            $table->json('settings')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
